e7a9bfdc3f42ab4966d7cd80bbe29a9e878d2118
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-10-12 09:11:36 +0600

    added redirection

--- src/components/TopicDetails/TopicDetails.js
+++ src/components/TopicDetails/TopicDetails.js
@@ -3,11 +3,11 @@ import React from 'react';
 const TopicDetails = ({ topic }) => {
     const { name, logo, total } = topic
     return (
-        <div className='border rounded'>
-            <h2 className='text-xl my-4'>Name: {name}</h2>
-            <img className='h-64 mx-auto w-full' src={logo} alt="" />
-            <p className='mt-4'>Question: {total}</p>
-            <button className='bg-slate-500 hover:bg-slate-600 rounded-md p-2 my-4 text-white font-bold'>Start Quiz</button>
+        <div className='border rounded bg-slate-200 text-center'>
+            <img className='h-64 mx-auto w-10/12' src={logo} alt="" />
+            <h2 className='text-xl my-2 font-bold'>{name}</h2>
+            <p className='mt-2'><small>Total Question: {total}</small></p>
+            <button className='bg-slate-500 hover:bg-slate-600 rounded-md p-2 my-2 text-white font-semibold'>Start Practice</button>
         </div>
     );
 };
